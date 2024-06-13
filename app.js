d601538836276d59e7c56794af6be924e4b556cc
Justin8606 <justinshajuk@gmail.com>
2024-06-14 02:03:56 +0530

    signin completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 
 const bcryptjs = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 
 const user = require("./models/user")
@@ -32,6 +33,37 @@ app.post("/signup",async(req,res)=>{
     // res.send("signup page")
     console.log(user)
     res.json({"status":"success"})
+})
+
+app.post("/signin",(req,res)=>{
+    let input = req.body
+    userModel.find({"email":input.email}).then(
+      (response)=>{
+          if (response.length>0) {
+              let dbpassword = response[0].password
+              bcryptjs.compare(input.password,dbpassword,(error,isMatch)=>{
+                  if (isMatch) {
+                      jwt.sign({email:input.email},"ksrtc-app",{expiresIn:"1d"},
+                          (error,token)=>{
+                          if (error) {
+                              res.json({"status":"unable to create token"})
+                          } else {
+                              res.json({"status":"succes","userId":response[0]._id,"token":token})
+                          }
+                      })
+                  } else {
+                      res.json({"status":"Invalid credentials"})
+                  }
+              })
+          } else {
+              res.json({"status":"user not found"})
+          }
+      }
+    ).catch(
+      (error)=>{
+          res.json(error)
+      }
+    )  
 })
 
 app.listen(8080,()=>{
