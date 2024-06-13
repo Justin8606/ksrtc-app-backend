64b79b2968cfbb8143d70cda823f8f1811662897
Justin8606 <justinshajuk@gmail.com>
2024-06-14 01:48:37 +0530

    signup completed

--- app.js
+++ app.js
@@ -2,16 +2,32 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 
+const bcryptjs = require("bcryptjs")
+
+
 const user = require("./models/user")
 const {userModel} = require("./models/user")
 
 const app = express()
+
+const generateHashedPassword = async(password)=>{
+    const salt = await bcryptjs.genSalt(10)
+    return bcryptjs.hash(password,salt)  
+}
+
 app.use(cors())
 app.use(express.json())
 
-app.post("/signup",(req,res)=>{
+mongoose.connect("mongodb+srv://justin:nitsuj21@cluster0.3jf2qw3.mongodb.net/tiketappdb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/signup",async(req,res)=>{
     let input = req.body
+
+    let hashedPassword = await generateHashedPassword(input.password)
+    input.password = hashedPassword
+
     let user = new userModel(input)
+    user.save()
     // console.log(input)
     // res.send("signup page")
     console.log(user)
